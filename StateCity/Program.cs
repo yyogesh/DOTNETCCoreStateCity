5bc1ee8b70400077802eb71457c3ff9f70e702ee
yyogesh <yogeshggn@hotmail.com>
2023-02-21 12:13:26 +0530

    FileController

--- StateCity/Program.cs
+++ StateCity/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(optons =>
+{
+    optons.ReturnHttpNotAcceptable= true;
+}).AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
